e54f9575d8ad19495ce9adde92b0339da645b906
Douglas <contato.cunhadoug@gmail.com>
2025-04-21 21:58:21 -0300

    init project

---
+++ src/main/java/br/edu/cads/helloworldspring/HelloWorldSpringApplication.java
@@ -0,0 +1,13 @@
+package br.edu.cads.helloworldspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldSpringApplication.class, args);
+    }
+
+}
---
+++ src/main/java/br/edu/cads/helloworldspring/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package br.edu.cads.helloworldspring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Spring Boot!";
+    }
+}
